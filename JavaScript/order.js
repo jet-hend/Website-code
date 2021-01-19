62b9b969ebee8240b7d62a75f1a8fd461b7de7f2
jet-hend <jhendrick196@gmail.com>
2021-01-19 14:16:10 -0500

    launch\

--- JavaScript/order.js
+++ JavaScript/order.js
@@ -96,3 +96,22 @@ function showSlides(n) {
 }
 
 //slide show end
+
+//order start
+
+var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbxYLn8qUQnHoiZfN6l2CvBi9jbJYQOCex7Vx_cKqJb5pd-mYa9Qo3pG/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+  );
+})
+
+//order end
